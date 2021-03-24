33e71a825cde66a3808f7839721af02e17ae9d30
Joe de Tassanyi <jdetass@gmail.com>
2021-03-24 23:32:55 +0000

    adding new dashboard view

--- budget_app/urls.py
+++ budget_app/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 app_name = 'budget_app'
 urlpatterns = [
-    path('', views.nada, name='nada'),
+    path('', views.budget_dashboard, name='budget_dashboard'),
 ]
--- budget_app/views.py
+++ budget_app/views.py
@@ -2,8 +2,21 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import PayPeriod, FixedExpense, Envelopes, NonFixedExpense
 #from .forms import ExpenseForm
 
-def nada(request):
+def budget_dashboard(request):
+    current_period = PayPeriod.objects.latest('start_date')
+    context = {
+            'current_period':current_period
+            }
+
+    return render(request, 'budget_app/dashboard.html', context)
+
+
+
+
+def new_pay_period(request):
     pass
+
+
 
 
 #def expense_list(request):
